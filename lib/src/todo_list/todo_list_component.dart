a0d24d8ffc13603ba847cd24bb740a2e3ae93ca0
wusonger <2216164015@qq.com>
2018-10-31 23:15:27 +0800

    第二次作业

--- lib/src/todo_list/todo_list_component.dart
+++ lib/src/todo_list/todo_list_component.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'dart:math';
+import 'dart:html';
 
 import 'package:angular/angular.dart';
 import 'package:angular_components/angular_components.dart';
@@ -14,28 +16,26 @@ import 'todo_list_service.dart';
     MaterialFabComponent,
     MaterialIconComponent,
     materialInputDirectives,
+    MaterialButtonComponent,
     NgFor,
     NgIf,
   ],
   providers: [const ClassProvider(TodoListService)],
 )
 class TodoListComponent implements OnInit {
-  final TodoListService todoListService;
-
-  List<String> items = [];
-  String newTodo = '';
-
-  TodoListComponent(this.todoListService);
+  
 
   @override
   Future<Null> ngOnInit() async {
-    items = await todoListService.getTodoList();
+    
   }
 
   void add() {
-    items.add(newTodo);
-    newTodo = '';
+    var random=Random();
+    var number=random.nextInt(12);
+    var lists=["李典康","张静雅","蔡心蕊","陈瑶","赵世宇","龙晶毅","戚晓颖","周嘉翔","吴松二","朱子恒","郑可欣","唐莉雯"];
+    querySelector('#tips').text="请"+lists[number]+"同学回答问题！";
   }
 
-  String remove(int index) => items.removeAt(index);
+
 }
