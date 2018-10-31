3f8cf16c5b6a8b45f6a95660a6f0925fb7753326
wusonger <2216164015@qq.com>
2018-10-31 23:31:57 +0800

    第二次作业

--- web/main.dart
+++ web/main.dart
@@ -1,6 +1,7 @@
 import 'package:angular/angular.dart';
 import 'package:wu_10164507127/app_component.template.dart' as ng;
-
+import 'dart:math';
+import 'dart:html';
 void main() {
   runApp(ng.AppComponentNgFactory);
 }
